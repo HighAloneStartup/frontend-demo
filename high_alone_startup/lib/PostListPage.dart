f5cdc72e21cd8fb4c9fb4371f16f80e7163c7723
정동원 <jdw1042710@gmai.com>
2022-08-12 23:47:45 +0900

    Implemented PostWrite Screen
    Except For Interaction

--- high_alone_startup/lib/PostListPage.dart
+++ high_alone_startup/lib/PostListPage.dart
@@ -3,6 +3,166 @@ import './PostWritePage.dart';
 import './component/MainTitle.dart';
 import './component/SubTitle.dart';
 
+class PostListPage extends StatelessWidget {
+  Widget _title() {
+    return Container(
+      padding: const EdgeInsets.only(top: 35, bottom: 35),
+      child: Column(
+        children: const [
+          MainTitle(
+            title: "FREE BOARD",
+            theme: Colors.white,
+          ),
+          SubTitle(
+            title: "자유게시판",
+            theme: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return Expanded(
+      //height: double.infinity,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            child: ListView.separated(
+              itemCount: _postList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _postList[index];
+              },
+              separatorBuilder: (BuildContext context, int index) {
+                return const Divider(
+                  thickness: 1,
+                  height: 1,
+                  color: Colors.white,
+                );
+              },
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(148, 34),
+                primary: Colors.black,
+                side: const BorderSide(
+                  color: Colors.white,
+                  width: 0.5,
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PostWritePage()),
+                );
+              },
+              child: Text("글쓰기"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  PostListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Column(
+          children: [
+            _title(),
+            _body(context),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Post extends StatelessWidget {
+  final String? _name;
+  final String? _photo;
+  final String _content;
+
+  _Post({
+    Key? key,
+    required int id,
+    required String content,
+  })  : _name = users[id]?['name'] as String,
+        _photo = 'images/${users[id]?['photo'] as String}',
+        _content = content,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var photo = Container(
+      width: 60,
+      margin: const EdgeInsets.only(right: 5),
+      height: 60,
+      child: (_photo == null)
+          ? Image.asset('images/default.jpg')
+          : Image.asset('images/default.jpg'), //Image.asset(_photo as String),
+      //-> default.jpg외에 다른 사진들이 문제인 것 같아 임의로 지정함.
+    );
+    var contents = Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SubTitle(
+            title: (_name == null) ? "익명" : _name as String,
+            theme: Colors.white,
+          ),
+          SubTitle(
+            title: _content,
+            theme: Colors.white,
+          ),
+        ],
+      ),
+    );
+
+    var activate = Container(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        children: const [
+          Icon(
+            size: 10,
+            Icons.thumb_up,
+            color: Colors.white,
+          ),
+          Icon(
+            size: 10,
+            Icons.comment,
+            color: Colors.white,
+          ),
+          Icon(
+            size: 10,
+            Icons.star,
+            color: Colors.white,
+          ),
+        ],
+      ),
+    );
+
+    return Container(
+      padding: const EdgeInsets.all(5),
+      width: double.infinity,
+      height: 70,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [photo, contents, activate],
+      ),
+    );
+  }
+}
+
+/// 테스트용 데이터
 List<_Post> _postList = [
   _Post(
     id: 0x0000,
@@ -87,161 +247,7 @@ List<_Post> _postList = [
 ];
 
 Map<int, Map<String, String>> users = {
-  0x0000: {'name': '손승표', 'photo': '손승표.jpg'},
-  0x0001: {'name': '정동원', 'photo': '정동원.jpg'},
-  0x0002: {'name': '황서진', 'photo': '황서진.jpg'},
+  0x0000: {'name': '손승표', 'photo': 'SSP.jpg'},
+  0x0001: {'name': '정동원', 'photo': 'JDW.jpg'},
+  0x0002: {'name': '황서진', 'photo': 'HSJ.jpg'},
 };
-
-class PostListPage extends StatelessWidget {
-  final Widget _title = Container(
-    padding: const EdgeInsets.only(top: 35, bottom: 35),
-    child: Column(
-      children: const [
-        MainTitle(
-          title: "FREE BOARD",
-          theme: Colors.white,
-        ),
-        SubTitle(
-          title: "자유게시판",
-          theme: Colors.white,
-        )
-      ],
-    ),
-  );
-
-  //final Widget _body =
-
-  PostListPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Container(
-        child: Column(
-          children: [
-            _title,
-            Expanded(
-              //height: double.infinity,
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    child: ListView.separated(
-                      itemCount: _postList.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return _postList[index];
-                      },
-                      separatorBuilder: (BuildContext context, int index) {
-                        return const Divider(
-                          thickness: 1,
-                          height: 1,
-                          color: Colors.white,
-                        );
-                      },
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 40,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        fixedSize: Size(148, 34),
-                        primary: Colors.black,
-                        side: const BorderSide(
-                          color: Colors.white,
-                          width: 0.5,
-                        ),
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PostWritePage()),
-                        );
-                      },
-                      child: Text("글쓰기"),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class _Post extends StatelessWidget {
-  final String? _name;
-  final String? _photo;
-  final String _content;
-
-  _Post({
-    Key? key,
-    required int id,
-    required String content,
-  })  : _name = users[id]?['name'] as String,
-        _photo = 'images/${users[id]?['photo'] as String}',
-        _content = content,
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    var photo = SizedBox(
-      width: 60,
-      height: 60,
-      child: (_photo == null)
-          ? Image.asset('images/default.jpg')
-          : Image.asset(_photo as String),
-    );
-    var contents = Expanded(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SubTitle(
-            title: (_name == null) ? "익명" : _name as String,
-            theme: Colors.white,
-          ),
-          SubTitle(
-            title: _content,
-            theme: Colors.white,
-          ),
-        ],
-      ),
-    );
-
-    var activate = Container(
-      alignment: Alignment.bottomCenter,
-      child: Row(
-        children: const [
-          Icon(
-            size: 10,
-            Icons.thumb_up,
-            color: Colors.white,
-          ),
-          Icon(
-            size: 10,
-            Icons.comment,
-            color: Colors.white,
-          ),
-          Icon(
-            size: 10,
-            Icons.star,
-            color: Colors.white,
-          ),
-        ],
-      ),
-    );
-
-    return Container(
-      padding: const EdgeInsets.all(5),
-      width: double.infinity,
-      height: 70,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [photo, contents, activate],
-      ),
-    );
-  }
-}
--- high_alone_startup/lib/PostWritePage.dart
+++ high_alone_startup/lib/PostWritePage.dart
@@ -3,23 +3,109 @@ import './component/MainTitle.dart';
 import './component/SubTitle.dart';
 
 class PostWritePage extends StatelessWidget {
-  final Widget _title = Container(
-    padding: const EdgeInsets.only(top: 35, bottom: 35),
-    child: Column(
-      children: const [
-        MainTitle(
-          title: "FREE BOARD",
-          theme: Colors.white,
-        ),
-        SubTitle(
-          title: "자유게시판",
-          theme: Colors.white,
-        ),
-      ],
-    ),
-  );
+  Widget _title() {
+    return Container(
+      padding: const EdgeInsets.only(top: 35, bottom: 35),
+      child: Column(
+        children: const [
+          MainTitle(
+            title: "FREE BOARD",
+            theme: Colors.white,
+          ),
+          SubTitle(
+            title: "자유게시판",
+            theme: Colors.white,
+          ),
+        ],
+      ),
+    );
+  }
 
-  //final Widget _body =
+  Widget _body(BuildContext context) {
+    return Expanded(
+      child: Column(
+        children: [
+          // head
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(5),
+            height: 70,
+            color: Colors.white,
+            child: Row(
+              children: [
+                Container(
+                  height: 60,
+                  width: 60,
+                  child: Image.asset('images/default.jpg'),
+                ),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.all(5),
+                          child: const TextField(),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          const Icon(
+                            Icons.camera_alt,
+                            size: 20,
+                          ),
+                          Checkbox(
+                            value: false,
+                            onChanged: (bool? value) {
+                              print("ischecked");
+                            },
+                          ),
+                          const Text("익명"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          //textbox
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(20),
+              color: Colors.white,
+              child: const TextField(
+                maxLines: null,
+                decoration: InputDecoration(
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          //button
+          Container(
+            margin: const EdgeInsets.all(40),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(148, 34),
+                primary: Colors.black,
+                side: const BorderSide(
+                  color: Colors.white,
+                  width: 0.5,
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("글쓰기 완료"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   PostWritePage({Key? key}) : super(key: key);
 
@@ -28,41 +114,10 @@ class PostWritePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Container(
-        height: double.infinity,
         child: Column(
           children: [
-            _title,
-            Container(
-              child: Column(
-                children: [
-                  Container(
-                    height: 100,
-                    margin: EdgeInsets.all(10),
-                    color: Colors.white,
-                    child: const TextField(
-                      maxLines: null,
-                      decoration: InputDecoration(
-                        focusedBorder: InputBorder.none,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: Size(148, 34),
-                      primary: Colors.black,
-                      side: const BorderSide(
-                        color: Colors.white,
-                        width: 0.5,
-                      ),
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text("글쓰기"),
-                  ),
-                ],
-              ),
-            ),
+            _title(),
+            _body(context),
           ],
         ),
       ),
