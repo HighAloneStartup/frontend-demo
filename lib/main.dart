e646dd22a64f896d981fa2e97d632f1e724dbeb4
정동원 <jdw1042710@gmai.com>
2022-11-04 09:46:56 +0900

    Update Configure

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home:
-          StudentListPage(), //LoginPage(), // StudentListPage(), // PostListPage(),
+        PostListPage(), // StudentListPage(), // PostListPage(), // LoginPage(),
     );
   }
 }
