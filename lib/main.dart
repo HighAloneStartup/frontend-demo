b2a8b9e9eb2a87929ce6892eee05e257a6244d56
swiftie1230 <swiftie1230@naver.com>
2022-11-15 17:27:09 +0900

    changed bug

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-      home: LoginPage(),
     return MaterialApp(
+      home: LoginPage(),
     );
   }
 }
