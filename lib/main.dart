dff527352cf1f661a27e444c1cc8cea959b84282
jdw1042710 <jdw1042710@gmail.com>
2022-11-24 20:29:25 +0900

    feat: Implement firebase environment

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'loginPage.dart';
-import 'board_list_page.dart';
-import 'class_board_page.dart';
-import 'new_post_page.dart';
-import 'student_list_page.dart';
-import './time_table_page.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
